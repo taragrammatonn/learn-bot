8df6d660da3c84797eda8f8f754cefea44c96ea0
taragrammatonn <qweqwe@qwe.qwe>
2024-06-23 18:21:08 +0300

    Customer Product Service Gateway

---
+++ customer-product-service-gateway/src/main/java/com/example/CustomerProductServiceGateway.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProductServiceGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerProductServiceGateway.class, args);
+    }
+}
---
+++ customer-product-service-gateway/src/test/java/com/example/HealthCheck.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HealthCheck {
+
+    @LocalServerPort
+    int port;
+
+    WebTestClient client;
+
+    @BeforeEach
+    void setUp() {
+        client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port).build();
+    }
+
+    @Test
+    void healthCheck() {
+        client.get().uri("/actuator/health")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .isEqualTo("{\"status\":\"UP\"}");
+    }
+}
