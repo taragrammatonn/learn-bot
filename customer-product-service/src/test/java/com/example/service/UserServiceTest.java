856968c6754dc0f5cdf4a883e40dd27a16496b8b
taragrammatonn <qweqwe@qwe.qwe>
2024-06-29 16:23:40 +0300

    Test containers init

--- customer-product-service/src/test/java/com/example/service/UserServiceTest.java
+++ customer-product-service/src/test/java/com/example/service/UserServiceTest.java
@@ -2,43 +2,77 @@ package com.example.service;
 
 import com.example.commons.integratiom.test.IntegrationTest;
 import com.example.dao.UserDAO;
+import com.example.dao.model.User;
 import com.example.factory.UserFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@IntegrationTest(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@Testcontainers
+@IntegrationTest
 class UserServiceTest {
+
+    static PostgreSQLContainer<?> postgresContainer =
+            new PostgreSQLContainer<>("postgres:15-alpine");
 
     @Autowired
     private TelegramUserService telegramUserService;
-
-    @MockBean
-    private UserDAO userDAO;
-
     @Autowired
     private WebApplicationContext webApplicationContext;
+    @Autowired
+    private UserDAO userDAO;
+
     private MockMvc mvc;
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+
+    @BeforeEach
+    void setUp() {
+        userDAO.deleteAll();
+
+        userDAO.save(
+                User.builder()
+                        .id(1L)
+                        .lName("qwe")
+                        .fName("qwe")
+                        .build()
+        );
+    }
 
     @BeforeEach
     public void setup() {
         this.mvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
                 .build();
+    }
+
+    @Test
+    void shouldReturnTestUserFromDB() {
+        assertThat(userDAO.findById(1L)).isPresent();
     }
 
     @Test
@@ -55,5 +89,15 @@ class UserServiceTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(APPLICATION_JSON))
                 .andExpect(content().json(userJson, true));
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        postgresContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgresContainer.stop();
     }
 }
